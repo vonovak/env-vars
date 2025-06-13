03e8721839325090bb5b0c077b3d2d784ad88535
Vojtech Novak <vonovak@gmail.com>
2025-06-13 17:45:47 +0200

    add OTHER_SECRET

--- app/getSecret+api.ts
+++ app/getSecret+api.ts
@@ -2,5 +2,6 @@ export function GET(request: Request) {
   return Response.json({
     secret: process.env.MY_SECRET,
     sensitive: process.env.MY_SENSITIVE,
+    otherSecretValue: process.env.OTHER_SECRET,
   });
 }
