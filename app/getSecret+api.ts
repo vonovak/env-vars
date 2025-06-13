aece267d89fc96ed792a428db6bc050e36fffcc1
Vojtech Novak <vonovak@gmail.com>
2025-06-13 18:44:04 +0200

    throw

--- app/getSecret+api.ts
+++ app/getSecret+api.ts
@@ -1,7 +1,3 @@
 export function GET(request: Request) {
-  return Response.json({
-    secret: process.env.MY_SECRET,
-    sensitive: process.env.MY_SENSITIVE,
-    otherSecretValue: process.env.OTHER_SECRET,
-  });
+  throw new Error("500");
 }
